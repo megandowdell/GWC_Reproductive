91cd416aa8eb083979a60f53509a6fc1f74589be
shrncr <loveyawink@gmail.com>
2025-03-05 11:38:02 -0600

    initial setup for gwc project

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+/*
+Root file for backend. Brings together API endpoints, defines port number, and connects to database
+*/
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./conn");
+
+const app = express();
+
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://printhub-eight.vercel.app'] ,//allow requests from frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  allowedHeaders: ['Content-Type', 'Authorization', 'User-Agent', 'Accept', 'Referer'], 
+  credentials: true, //cookies
+}));
+
+app.use(express.json());
+connectDB(); 
+
+//oh crud ops
+// app.use('/users', require('./routes/user'));
+// app.use('/listings', require('./routes/listing'));
+// app.use('/purchases', require('./routes/purchase'));
+
+
+
+
+const port = process.env.PORT || 8082;
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running on port: ${port}`);
+});
+
+
+module.exports = app;
